6b2fa75c22eaab116616c9251de9a9f502f9e669
RyleZamayla <joshuaryle4107@gmail.com>
2023-10-15 22:01:54 +0800

    added css hyperlinks for FullCalendar.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,5 +29,8 @@
             calendar.render();
         });
     </script>
+     <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/core/main.css" rel="stylesheet" />
+     <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid/main.css" rel="stylesheet" />
+     <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/timegrid/main.css" rel="stylesheet" />
     @endpush
 </x-app-layout>
